fa0bcdd089ecf29d1023e57c3c51ec2494e6bd54
Cesar David Millan <cesardavidmillan5@gmail.com>
2025-09-10 05:15:50 -0500

    Working on chapter 7 about inputs and while loops

---
+++ Chapter_7_User_Input_And_While_Loops/rollercoaster/rollercoaster.py
@@ -0,0 +1,12 @@
+height = int(input("How tall are you, in inches? "))
+
+if height >= 48:
+    print("\nYou're tall enough to ride")
+else:
+    print("\nYou'll be able to ride when you're little older.")
+
+# When you use numerical input to do calculations and comparisons, be sure to convert the input value to a numerical
+# representation first
+
+
+
