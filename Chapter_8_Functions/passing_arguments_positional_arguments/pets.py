effefd67428b64a0b1a6a1b345145854e087fbb3
Cesar David Millan <cesardavidmillan5@gmail.com>
2025-09-13 06:50:10 -0500

    mixing up some concepts about functions and while loops

--- Chapter_8_Functions/passing_arguments_positional_arguments/pets.py
+++ Chapter_8_Functions/passing_arguments_positional_arguments/pets.py
@@ -1,9 +1,24 @@
-user_animal_type = input("Tell me what kind of pet do you have? ")
-user_pet_name = input("What's your pets name? ")
-
 def describe_pet(animal_type, pet_name):
     """Display information about your pet!"""
     print(f"\nI have a {animal_type}.")
     print(f"My {animal_type}'s name is {pet_name.title()}.")
 
-describe_pet(user_animal_type, user_pet_name)
+#describe_pet(user_animal_type, user_pet_name)
+
+def pet_information():
+    """Ask for your pet name and type of animal an pass the argument though the describe pets function"""
+    user_animal_type = input("Tell me what kind of pet do you have? ")
+    user_pet_name = input("What's your pets name? ")
+    describe_pet(user_animal_type, user_pet_name)
+
+#pet_information()
+#pet_information()
+
+def pet_polling():
+    """Enter various time information about several pets"""
+    amount_pets = int(input("How many pets do you have? "))
+    while amount_pets > 0:
+        pet_information()
+        amount_pets -= 1
+
+pet_polling()
