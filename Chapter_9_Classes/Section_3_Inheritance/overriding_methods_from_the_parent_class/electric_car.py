bf491a91f9159493ca59144ae9fb8f78659c47d7
Cesar David Millan <cesardavidmillan5@gmail.com>
2025-10-01 18:41:27 -0500

    working on class instances and attributes

---
+++ Chapter_9_Classes/Section_3_Inheritance/overriding_methods_from_the_parent_class/electric_car.py
@@ -0,0 +1,65 @@
+class Car:
+    """A simple attempt to represent a car."""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+        self.gas_tank = 50
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        """Print a statement showing the car's milage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, milage):
+        """
+        Set the odometer reading to given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if milage >= self.odometer_reading:
+            self.odometer_reading = milage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
+    def fill_gas_tank(self):
+        """Add gas to the tank of the vehicle """
+        #if gas_amount >= self.gas_tank:
+            #self.gas_tank = gas_amount
+        print(f"The vehicle has {self.gas_tank} lts of gas.")
+        #else:
+            #print("You have no gas in the tank, the engine won't start!")
+
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+
+    def __init__(self,make,model,year):
+        """
+        Initialize attributes of the parent class.
+        Then initialize attributes specific to an electric car.
+        """
+        super().__init__(make,model,year)
+        self.battery_size = 40
+
+    def describe_battery(self):
+        """print a statement describing the battery size."""
+        print(f"Th car has a {self.battery_size}--kWh battery.")
+
+    def fill_gas_tank(self):
+        """Electric cars don't have gas tanks."""
+        print("This car doesn't have a gas tank!")
+
+my_leaf = ElectricCar('nissan','leaf',2024)
+print(my_leaf.get_descriptive_name())
+my_leaf.describe_battery()
+my_leaf.fill_gas_tank()
