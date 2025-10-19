c2d6e93c6c73d7ff73d8a2ca60d7f4a7cd1ada02
Cesar David Millan <cesardavidmillan5@gmail.com>
2025-10-19 06:24:17 -0500

    almost finish with chapter 15

--- Chapter_15_Generating_Data/Random_Walks/Creating_a_RandomWalk_Class/random_walk.py
+++ Chapter_15_Generating_Data/Random_Walks/Creating_a_RandomWalk_Class/random_walk.py
@@ -1,5 +1,8 @@
 from random import choice
 
+
+
+
 class RandomWalk:
     """A class to generate random walks."""
 
@@ -11,21 +14,33 @@ class RandomWalk:
 
         self.x_values = [0]
         self.y_values = [0]
+
+
+    def get_step(self):
+        """Method to determine the direction and distance for each step."""
+        direction = choice([1, -1])
+        distance = choice(range(9))
+        step = direction * distance
+        return step
+
     def fill_walk(self):
         """Calculate all the points in the walk."""
 
-        # Keep taking steps until the walk reaches the desire lenght.
+        # Keep taking steps until the walk reaches the desire length.
 
         while len(self.x_values) < self.num_points:
 
-            # Decide which direction to go, and how far to go.
-            x_direction = choice([1, -1])
-            x_distance = choice([0,1,2,3,4])
-            x_step = x_direction * x_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
 
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            # Decide which direction to go, and how far to go.
+            #x_direction = choice([1, -1])
+            #x_distance = choice([0,1,2,3,4,5,6,7,8])
+            #x_step = x_direction * x_distance
+
+            #y_direction = choice([1, -1])
+            #y_distance = choice([0, 1, 2, 3, 4,5,6,7,8])
+            #y_step = y_direction * y_distance
 
             # Reject moves that go nowhere.
 
---
+++ Chapter_15_Generating_Data/Rolling_Dice_with_Plotly/die_visual.py
@@ -0,0 +1,29 @@
+import plotly.express as px
+from die import Die
+
+# create a D6.
+
+die = Die()
+
+# make some rolls, and store results in a list.
+results = []
+for roll_num in range(1000):
+    result = die.roll()
+    results.append(result)
+
+# Analyze the results.
+
+frequencies = []
+poss_results = range(1, die.num_sides + 1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the resutls.
+title = "Results of Rolling One D6 1,000 Times"
+labels = {'x': 'Result', 'y': 'Frequency of Results'}
+
+
+# Visualize the results.
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+fig.show()
